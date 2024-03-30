524b03a0e9a06951287b79e0464ae09350043411
Kuz <1424894032@qq.com>
2024-03-30 22:38:38 +0800

    3.30 22：30

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class CardManager : Singleton<CardManager>
 {   
-    public List<GameObject> normalCard;
-    public List<GameObject> specialCard;
-    public GameObject g1, g2;
+    public List<Card> normalCard;
+    public List<Card> specialCard;
+    public Card g1, g2;
     protected override void Awake()
     {
         base.Awake();
@@ -22,10 +22,12 @@ public class CardManager : Singleton<CardManager>
         if (Input.GetMouseButtonDown(0))
         {
             GetNormalCard(out g1, out g2);
+            LeanTween.rotate(g1.GetComponent<GameObject>(), new Vector3(0, 0, -5f), 1.5f).setEaseInOutBack();
+            LeanTween.moveLocal(g1.GetComponent<GameObject>(), new Vector3(0, 0, 0), 1.5f).setEaseInOutBack();
         }
     }
 
-    public GameObject GetNormalCard(out GameObject card1,out GameObject card2)
+    public Card GetNormalCard(out Card card1,out Card card2)
     {
         int i = Random.Range(0, 5);
         if(i<2)
@@ -35,7 +37,7 @@ public class CardManager : Singleton<CardManager>
             card2 = normalCard[2];
             Debug.Log("0");
         }
-        else if(i<4)
+        else if(i>=2&&i<4)
         {
             i = 1;
             card1 = normalCard[0];
@@ -51,9 +53,30 @@ public class CardManager : Singleton<CardManager>
         }
         return specialCard[i];
     }
-    public GameObject GetSpecialCard()
+    public Card GetSpecialCard()
     {
         return specialCard[Random.Range(0, 3)];
     }
 
+    /// <summary>
+    /// 判断玩家是否赢过怪物
+    /// </summary>
+    /// <param name="c1">怪物出的牌</param>
+    /// <param name="c2">玩家出的牌</param>
+    /// <returns></returns>
+    public bool CompareCardStats(Card c1,Card c2)
+    {
+        if(c1.cardStats==CardStats.rock&&c2.cardStats==CardStats.paper
+            ||c1.cardStats==CardStats.paper&&c2.cardStats==CardStats.scissors
+            ||c1.cardStats==CardStats.scissors&&c2.cardStats==CardStats.rock)
+        {
+            return true;
+        }
+        else 
+        { 
+            return false; 
+        }
+    }
+
 }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager> 
-{ 
-    
+{
+    public SceneStats _curStats;
+
+    private float bossAttackTime;
+    private float PlayerWaitTime;
+
+    private bool isPlayer1 = false;
+    private bool isPlayer2 = false;
+
+    private Card bossCard;
+    private Card playerCard1;
+    private Card playerCard2;
+    private void Update()
+    {
+        
+    }
+
+    private void Round()
+    {
+        switch(_curStats)
+        {
+            case SceneStats.BossRound1:
+                bossCard = CardManager.Instance.GetNormalCard(out playerCard1,out playerCard2);
+                break;
+            case SceneStats.BossRound2:
+
+                break;
+            case SceneStats.PlayerRound:
+
+                break;
+
+        }
+    }
 }
+
+public enum SceneStats    
+{
+    BossRound1,
+    BossRound2,
+    PlayerRound,
+}
+
