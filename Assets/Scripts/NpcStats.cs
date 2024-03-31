ae6b8be22c72395457596937938aa63b296db1de
W66Git66 <2120405615@qq.com>
2024-03-31 13:47:10 +0800

    no message

---
+++ Assets/Scripts/NpcStats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcStats : MonoBehaviour
+{
+    public float _curHp;
+    public float _MaxHp;
+    public int _curWealth;
+
+    public Sprite normal;
+    public Sprite getHurt;
+
+
+}
